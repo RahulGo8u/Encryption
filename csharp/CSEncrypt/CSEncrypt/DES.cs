d23d864ae06e2fe551a994f1c0133fbb3b919dcb
rahulgo8u <rrsimt.rahul@gmail.com>
2023-08-02 14:32:45 +0400

    Adding CSharp project for implementing different encryption methodologies

---
+++ csharp/CSEncrypt/CSEncrypt/DES.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSEncrypt
+{
+    //Symmetric algorithms
+    //Data Encryption Standard
+    //Uses same secret for encryption and decryption
+    // uses 56 bits of key for encryption
+    //key length must be of 8 characters
+    internal class DES
+    {
+        public static string Encrypt(string plainText, string secretKey)
+        {
+            if(secretKey.Length != 8)
+            {
+                throw new Exception("Key length must be of 8 characters");
+            }
+            else
+            {
+                var secretKeyArray = Encoding.UTF8.GetBytes(secretKey);
+                var ivArray = Encoding.UTF8.GetBytes(secretKey);
+                using (DSACryptoServiceProvider provider = new DSACryptoServiceProvider())
+                {
+                    //provider.key 
+                }
+            }
+            return "";
+        }
+    }
+}
